0da300003eb64a0e4923c7ba8838040d0d877a89
MD IMRAN HABIB <mdimranhabib5@gmail.com>
2019-11-04 15:39:33 +0600

    create layer architecture and model classes

---
+++ Stock Management System/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Stock_Management_System.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public int CompanyId { get; set; }
+
+        [Required]
+        public double ReorderLevel { get; set; }
+
+        [Required]
+        public double Quantity { get; set; }
+    }
+}
